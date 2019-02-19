26c2b5a1ec83173356f16dc8df8539b84328b086
Andria Micunovic <andriamicunovic@Andrias-MacBook-Pro.local>
2019-02-19 21:10:09 +0100

    Added imporived permissions to use profile feed items API

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -11,3 +11,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id #check if the user requesting a change is editing its profile
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow uder to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check user is trying to update their own status"""
+
+        if request.method in permissions.SAFE_METHODS: #check if the request is in safe permission method (not delete, destroy)
+            return True
+
+        return obj.user_profile.id == request.user.id
